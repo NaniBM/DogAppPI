c35be561b4c78ea99788080072c76ef03393cb27
NaniBM <bazanmurua@gmail.com>
2021-09-27 13:39:42 -0300

    -mModels added

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,53 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes, Sequelize } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      height: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        get() {
+          return { metric: this.getDataValue("height") };
+        },
+      },
+      weight: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        get() {
+          return { metric: this.getDataValue("weight") };
+        },
+      },
+      life_span: {
+        type: DataTypes.STRING,
+        get() {
+          return this.getDataValue("life_span") + " Years";
+        },
+      },
+      image: {
+        type: DataTypes.STRING,
+        validate: {
+          isUrl: true,
+        },
+        defaultValue:
+          "https://image.freepik.com/free-photo/cute-little-dog-impersonating-business-person_23-2148985938.jpg",
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
