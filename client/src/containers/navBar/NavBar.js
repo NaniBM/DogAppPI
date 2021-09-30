f53b3967ae69b3b9e0a2304804c8e935e008e11a
NaniBM <bazanmurua@gmail.com>
2021-09-30 02:25:24 -0300

    -mAdded CSS to the NavBar

--- client/src/containers/navBar/NavBar.js
+++ client/src/containers/navBar/NavBar.js
@@ -1,22 +1,30 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./NavBar.css";
-import Logo from "../../images/landing.jpg";
+import logo from "../../images/Logo.png";
+import addPaw from "../../images/add.png";
 
 export default function NavBar() {
   return (
     <div className="NavBar">
-      <div className="Logo">
-        
-        <NavLink to="/home">
-          <img width="36" height="36" src={Logo} alt="DogApp" />
-          <span>DogApp</span>
-        </NavLink>
-      
+      <div className="home">
+        <Link className="textLogo" to="/home">
+          <img src={logo} className="homeLogo" alt="logo: a dog´s house" />
+          <div className="textLogo">
+            <h1 className="NavLogo">Good-Boy </h1>
+            <h1 className="NavLogo">Good-Boy</h1>
+          </div>
+        </Link>
       </div>
-      <div className="CreateBreed">
-        <NavLink to="/addbreed">Add new breed</NavLink>
+      <div className="AddBreed">
+        <Link className="addLink" to="/addbreed">
+          <img className="addPaw" src={addPaw} alt="The paw of a dog" />
+        </Link>
+        <Link className="addLink" to="/addbreed">
+          Add new breed
+        </Link>
       </div>
+      <div className="endNavLine"></div>
     </div>
   );
 }
